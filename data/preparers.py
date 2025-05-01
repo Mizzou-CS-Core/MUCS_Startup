b42f9a700d0287730165e376760795a36a64a386
Matt Marlow <thequantumblink@gmail.com>
2025-04-30 21:20:46 -0500

    feat: updated usage of gen assignment

--- data/preparers.py
+++ data/preparers.py
@@ -1,5 +1,5 @@
 import logging
-from gen_assignment_window.gen_lab_window import initialize_window
+from gen_assignment_window.gen_lab_window import prepare_assignment_window_and_config
 from configuration.config import Config
 from git import Repo
 from canvas_lms_api import get_client
@@ -8,13 +8,12 @@ logger = logging.getLogger(__name__)
 
 
 def prepare_assignment_table(config: Config):
-    cursor = get_cursor(config=config)
     canvas_assignment_name_predicate = input("Enter a canvas_assignment_name_predicate: ")
     blacklist = list(iter(lambda: input("Enter (empty to stop): "), ""))
     gen_table = config.data / "gen_assignment_table"
     logger.info(f"Config path is {gen_table}")
     gen_table.mkdir()
-    initialize_window(cursor=cursor, config_path=gen_table, canvas_token=config.api_token, canvas_course_id=config.course_ids[0], canvas_assignment_name_predicate=canvas_assignment_name_predicate, canvas_assignment_phrase_blacklist=blacklist, mucs_course_code=config.class_code)
+    prepare_assignment_window_and_config(config_path=gen_table, canvas_token=config.api_token, canvas_course_id=config.course_ids, canvas_assignment_name_predicate=canvas_assignment_name_predicate, canvas_assignment_phrase_blacklist=blacklist, mucs_instance_code=config.class_code, sqlite3_path=config.sqlite_db_path)
 def prepare_course_data(config: Config):
     dao.store_mucs_course()
     for course_id in config.course_ids:
