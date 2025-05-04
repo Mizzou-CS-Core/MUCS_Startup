0e79b23d13043d490fc0b8453684a393861bac78
Matt Marlow <thequantumblink@gmail.com>
2025-05-04 17:21:46 -0500

    refactor: cleaned up logging

--- data/preparers.py
+++ data/preparers.py
@@ -1,4 +1,5 @@
 import logging
+from colorama import Fore, Style
 from gen_assignment_window.gen_assignment_window import prepare_toml as prepare_assignment_toml, \
     prepare_assignment_window
 from gen_grader_table.grader_table import generate_all_rosters, prepare_toml as prepare_roster_toml
@@ -15,11 +16,13 @@ logger = logging.getLogger(__name__)
 
 def prepare_assignment_table():
     config = get_config()
-    download_script(config.githubpaths.gen_assignment_table, "gen_assignment_table")
-    canvas_assignment_name_predicate = input("Enter a canvas_assignment_name_predicate: ")
-    blacklist = list(iter(lambda: input("Enter blacklist phrases (empty to stop): "), ""))
-    gen_table = config.bin / "gen_assignment_table"
-    logger.info(f"Config path is {gen_table}")
+    name = "gen_assignment_table"
+    logger.info(f"Preparing a standalone copy of {name}. This may take a while!")
+    download_script(config.githubpaths.gen_assignment_table, directory_name=name)
+    canvas_assignment_name_predicate = input(f"{Fore.BLUE}Enter a canvas_assignment_name_predicate: {Style.RESET_ALL}")
+    blacklist = list(iter(lambda: input(f"{Fore.BLUE}Enter blacklist phrases (empty to stop): {Style.RESET_ALL}"), ""))
+    gen_table = config.bin / name
+    logger.debug(f"Config path is {gen_table}")
     prepare_assignment_toml(config_path=gen_table, canvas_token=config.api_token, canvas_course_id=config.course_ids[0],
                             canvas_assignment_name_predicate=canvas_assignment_name_predicate,
                             canvas_assignment_phrase_blacklist=blacklist, mucs_instance_code=config.class_code,
@@ -32,10 +35,11 @@ def prepare_assignment_table():
 
 def prepare_grading_table():
     config = get_config()
-    logger.info("Preparing grading tables")
-    download_script(config.githubpaths.gen_grader_table, "gen_grader_table")
-    gen_table = config.bin / "gen_grader_table"
-    logger.info(f"Config path is {gen_table}")
+    name = "gen_grader_table"
+    logger.info(f"Preparing a standalone copy of {name}. This may take a while!")
+    download_script(config.githubpaths.gen_grader_table, directory_name=name)
+    gen_table = config.bin / name
+    logger.debug(f"Config path is {gen_table}")
     prepare_roster_toml(mucsv2_instance_code=config.class_code, db_path=config.sqlite_db_path,
                         canvas_token=config.api_token, canvas_course_id=config.course_ids[0],
                         canvas_url_base=config.api_prefix, roster_invalidation_days=3, config_base=gen_table, )
